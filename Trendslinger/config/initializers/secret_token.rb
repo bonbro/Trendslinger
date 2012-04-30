6141b8ea5ce1914eecdf025e4fcb819847de8e1e
Rob Bonstein <r.bonstein@gmail.com>
2012-04-29 18:18:54 -0700

    rails app created

---
+++ Trendslinger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendslinger::Application.config.secret_token = '0d12761660c4783c6c22782393158e36bea746f25f20cbf863b3754a3f4e20c179ce8222f93441e078ba058e12015d996fb93d44684a7d0f3160bc0b1c16dd5c'
